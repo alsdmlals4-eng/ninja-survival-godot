d7fc26c6e83e8cc8a0a89c4111812b2cd6494172
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-09 08:02:01 +0900

    feat: define HUDController runtime contract

--- scripts/ui/hud.gd
+++ scripts/ui/hud.gd
@@ -1,2 +1,14 @@
 extends CanvasLayer
 class_name HUDController
+
+
+func set_health(_current: int, _maximum: int) -> void:
+	pass
+
+
+func set_score(_score: int, _kills: int) -> void:
+	pass
+
+
+func show_game_over() -> void:
+	pass
