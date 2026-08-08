e30e2f4f413004d9d9abaa14c0e456cf0bf19ef1
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-09 07:49:05 +0900

    feat: define AutoAttackController contract

--- scripts/combat/auto_attack_controller.gd
+++ scripts/combat/auto_attack_controller.gd
@@ -1,2 +1,9 @@
 extends Node
 class_name AutoAttackController
+
+@export var attack_interval: float = 0.8
+@export var projectile_scene: PackedScene
+
+
+func find_nearest_target(_candidates: Array, _origin: Vector2) -> Node2D:
+	return null
