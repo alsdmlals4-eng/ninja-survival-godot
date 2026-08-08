907dfcfe1e62bf0208a343543374eb74a6d8485a
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-09 07:49:10 +0900

    feat: define BasicProjectile contract

--- scripts/combat/projectile.gd
+++ scripts/combat/projectile.gd
@@ -1,2 +1,10 @@
 extends Area2D
 class_name BasicProjectile
+
+var direction: Vector2 = Vector2.ZERO
+@export var speed: float = 500.0
+@export var damage: int = 10
+
+
+func configure(_new_direction: Vector2, _new_speed: float, _new_damage: int) -> void:
+	pass
