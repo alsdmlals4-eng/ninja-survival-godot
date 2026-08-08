bd00bbd2678a93d17b49dd003f81f60d5f0c0cd1
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-09 08:12:06 +0900

    test: cover enemy contact damage cooldown

--- tests/unit/test_enemy_chaser.gd
+++ tests/unit/test_enemy_chaser.gd
@@ -2,6 +2,14 @@ extends GutTest
 
 const EnemyScript = preload("res://scripts/enemies/enemy_chaser.gd")
 
+class DamageTarget:
+	extends Node2D
+	var damage_taken: int = 0
+
+	func take_damage(amount: int) -> void:
+		damage_taken += amount
+
+
 var death_count: int = 0
 
 
@@ -37,6 +45,27 @@ func test_set_target_stores_target() -> void:
 	enemy.set_target(target)
 
 	assert_eq(enemy.target, target)
+
+
+func test_contact_damage_is_cooldown_gated() -> void:
+	var enemy = EnemyScript.new()
+	enemy.contact_damage = 7
+	enemy.contact_cooldown = 0.75
+	add_child_autofree(enemy)
+	var target = DamageTarget.new()
+	add_child_autofree(target)
+	enemy.set_target(target)
+	enemy.global_position = Vector2.ZERO
+	target.global_position = Vector2.ZERO
+
+	enemy._physics_process(0.0)
+	assert_eq(target.damage_taken, 7)
+
+	enemy._physics_process(0.1)
+	assert_eq(target.damage_taken, 7)
+
+	enemy._physics_process(0.65)
+	assert_eq(target.damage_taken, 14)
 
 
 func _on_enemy_died(_enemy: Node) -> void:
