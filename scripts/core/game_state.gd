adac80c11ebc95443a643bcea467c5b93ce0c47b
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-09 07:48:39 +0900

    feat: define GameState contract

--- scripts/core/game_state.gd
+++ scripts/core/game_state.gd
@@ -1,2 +1,11 @@
 extends Node
 class_name GameState
+
+signal score_changed(score: int, kill_count: int)
+
+var score: int = 0
+var kill_count: int = 0
+
+
+func register_kill(_points: int = 100) -> void:
+	pass
