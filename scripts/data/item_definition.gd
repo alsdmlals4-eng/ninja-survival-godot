6c5d467b7a58b493ff31dacec554299a8d5b7ce0
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-24 12:56:58 +0900

    feat: extend item definition for spatial catalog data

--- scripts/data/item_definition.gd
+++ scripts/data/item_definition.gd
@@ -1,6 +1,8 @@
 extends Resource
 class_name ItemDefinition
 
+const RunModifierSetScript = preload("res://scripts/data/run_modifier_set.gd")
+
 @export var id: StringName = &""
 @export var display_name: String = ""
 @export var base_price: int = 0
@@ -8,7 +10,29 @@ class_name ItemDefinition
 @export var effect_kind: StringName = &""
 @export var effect_value: float = 0.0
 @export var school_payload: Dictionary = {}
+@export var footprint_size: Vector2i = Vector2i.ONE
+@export var static_modifier_payload: Dictionary = {}
+@export var spatial_rules: Array[SpatialRuleDefinition] = []
 
 
 func sell_price() -> int:
 	return floori(float(maxi(base_price, 0)) / 2.0)
+
+
+func footprint(rotation_quarters: int) -> Array[Vector2i]:
+	var size := footprint_size
+	if posmod(rotation_quarters, 2) == 1:
+		size = Vector2i(size.y, size.x)
+	var result: Array[Vector2i] = []
+	for y in range(maxi(size.y, 0)):
+		for x in range(maxi(size.x, 0)):
+			result.append(Vector2i(x, y))
+	return result
+
+
+func resolved_static_modifier_payload() -> Dictionary:
+	if not static_modifier_payload.is_empty():
+		return static_modifier_payload.duplicate(true)
+	if RunModifierSetScript.is_supported_field(effect_kind):
+		return {effect_kind: effect_value}
+	return {}
