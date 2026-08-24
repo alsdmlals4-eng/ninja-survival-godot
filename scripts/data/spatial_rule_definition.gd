c3596a1a9c846a6a3742a9c22a0a72d5c6871735
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-24 12:56:20 +0900

    feat: add bounded spatial rule definition

---
+++ scripts/data/spatial_rule_definition.gd
@@ -0,0 +1,13 @@
+extends Resource
+class_name SpatialRuleDefinition
+
+enum Aggregation {
+	ONCE_IF_ANY,
+	PER_DISTINCT_NEIGHBOR,
+}
+
+@export var required_neighbor_tags: Array[StringName] = []
+@export var required_neighbor_definition_ids: Array[StringName] = []
+@export var aggregation: Aggregation = Aggregation.ONCE_IF_ANY
+@export var max_matches: int = 1
+@export var modifier_payload: Dictionary = {}
